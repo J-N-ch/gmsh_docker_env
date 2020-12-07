d39f6d5ec7ac7a155ef4c8034ec3cb4f2d800a61
J-N-ch <andrewt.chent.t@gmail.com>
2020-12-07 18:22:33 +0800

    [desc] add how to turn .vtk file to .stl or .msh

--- dockerVolumeDir/meshio_test/meshRead2Triangle.py
+++ dockerVolumeDir/meshio_test/meshRead2Triangle.py
@@ -8,5 +8,19 @@ mesh = meshio.read(
     file_format="vtk", # optional if filename is a path; inferred from extension
 )
 
+print(mesh.cells_dict)
 print(mesh.cells_dict['triangle'])
 
+points = mesh.points
+
+cells = [("triangle", mesh.cells_dict['triangle'])]
+meshio.write_points_cells(
+    "foo.msh",
+    #"foo.stl,
+    points,
+    cells,
+    # Optionally provide extra data on points, cells, etc.
+    # point_data=point_data,
+    # cell_data=cell_data,
+    # field_data=field_data
+)
